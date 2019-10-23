f0b971e7513348d80108cf50ec886238f72c5c9d
EmperorJack <emperorpurvis@gmail.com>
2019-10-23 17:33:25 +1300

    Updates

---
+++ precache-manifest.d92ce851f16c4b8d0e1851ee9ddec162.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c519883c7a7bf307bed4b6a58682d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14556101eaeadb9580c1",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "b1e60c3c1bca1a0d36e5",
+    "url": "/static/css/main.24af475e.chunk.css"
+  },
+  {
+    "revision": "14556101eaeadb9580c1",
+    "url": "/static/js/2.5ab7d8bb.chunk.js"
+  },
+  {
+    "revision": "b1e60c3c1bca1a0d36e5",
+    "url": "/static/js/main.62b70064.chunk.js"
+  },
+  {
+    "revision": "f5c300d6359eb8c7b66d",
+    "url": "/static/js/runtime-main.f19f1c65.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c40ad354e9ede27922b4f364683dc831",
+    "url": "/static/media/profile.c40ad354.png"
+  },
+  {
+    "revision": "31d6fcdf1a4e04a8c1861573c33702a1",
+    "url": "/static/media/screenshot_1541038272_35467.31d6fcdf.png"
+  },
+  {
+    "revision": "10a0080e09d89478e32f9d2b31166448",
+    "url": "/static/media/screenshot_1543479869_463215.10a0080e.png"
+  },
+  {
+    "revision": "b64a2d449aa4f9e5231d689489fab494",
+    "url": "/static/media/screenshot_1545120495_235768.b64a2d44.png"
+  },
+  {
+    "revision": "f1ac7fbc1154a8f2a6261775e5fc98b7",
+    "url": "/static/media/screenshot_1545865352_46967.f1ac7fbc.png"
+  },
+  {
+    "revision": "ebf8e995862818b0651c6e9fdbcc576c",
+    "url": "/static/media/screenshot_1551433412_70563.ebf8e995.png"
+  },
+  {
+    "revision": "39ceae91e86498bfb80d6fb7ad5b613e",
+    "url": "/static/media/screenshot_1551772931_267000.39ceae91.png"
+  },
+  {
+    "revision": "13c4e6a4f58853299ac81f42af30d6bd",
+    "url": "/static/media/screenshot_1554362622_247116.13c4e6a4.png"
+  },
+  {
+    "revision": "dae4912e46c4cdbe5e09d0c0106e60bd",
+    "url": "/static/media/screenshot_1559383921_383822.dae4912e.png"
+  },
+  {
+    "revision": "d3ad774775cf0f4b53bf5dd77e6a0a5e",
+    "url": "/static/media/screenshot_1562914521_11415.d3ad7747.png"
+  },
+  {
+    "revision": "c285b22aefc51c8dab796c91121b774a",
+    "url": "/static/media/screenshot_1565413092_715942.c285b22a.png"
+  },
+  {
+    "revision": "2ded3ce09b00d6daa8e7c45f95a2cc2c",
+    "url": "/static/media/screenshot_1567592143_608136.2ded3ce0.png"
+  },
+  {
+    "revision": "22280b8dd5d2bf6b72a4f8643a6f3a4f",
+    "url": "/static/media/screenshot_1567604174_41724.22280b8d.png"
+  },
+  {
+    "revision": "18ec1e28b5f9d236cf4fe787bd4785f4",
+    "url": "/static/media/screenshot_1567663328_190520.18ec1e28.png"
+  },
+  {
+    "revision": "25df2e33ead08ec0449091eb70728903",
+    "url": "/static/media/screenshot_1571392758_833374.25df2e33.png"
+  },
+  {
+    "revision": "acd858377783b466d50d2b67bdf3ec8c",
+    "url": "/static/media/smoke-simulation.acd85837.jpg"
+  },
+  {
+    "revision": "99071ab6473ac88e3a524183bb89bca2",
+    "url": "/static/media/visor.99071ab6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/emperorjack.github.io/precache-manifest.10d2fc39b03b5b4f8dfef3db1696e141.js"
+  "/precache-manifest.d92ce851f16c4b8d0e1851ee9ddec162.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/emperorjack.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
