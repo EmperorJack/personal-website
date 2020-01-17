718451d572d801d59ad0b0449cc92c752c37e5f9
EmperorJack <emperorpurvis@gmail.com>
2020-01-18 10:52:47 +1300

    Update meta tag description. Bump copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer__copyright">
-    Copyright © 2019 Jack Purvis - All Rights Reserved
+    Copyright © 2020 Jack Purvis - All Rights Reserved
   </div>
 );
 
