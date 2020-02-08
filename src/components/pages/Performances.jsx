ed69cbd1c1df5a6d91ffe7a915c976db05934887
EmperorJack <emperorpurvis@gmail.com>
2020-02-08 10:39:49 +0000

    Add ICLC 2020 algorave performance and VIU 2019 recording links

--- src/components/pages/Performances.jsx
+++ src/components/pages/Performances.jsx
@@ -5,11 +5,22 @@ import performances from '../../performances.json';
 
 /* eslint-disable quote-props */
 
+const urlList = (urls, urlLabels) => (
+  <Fragment>
+    {urls.map((url, i) => (
+      <Fragment key={url}>
+        <Link url={url}>({urlLabels[i]})</Link>
+        {' '}
+      </Fragment>
+    ))}
+  </Fragment>
+);
+
 const sections = {
   'VISOR': (<Fragment>
     <div className="list">
       {Object.keys(performances).reverse().map((year) => (
-        <Fragment>
+        <Fragment key={year}>
           <div className="title">{year}</div>
 
           {performances[year].map((performance) => {
@@ -18,14 +29,14 @@ const sections = {
             } = performance;
 
             return (
-              <div className="list-item">
+              <div className="list-item" key={name}>
                 [{date}]
                 {' '}
                 <strong>{name}</strong>,
                 {' '}
                 {location}
                 {' '}
-                {url && (<Link url={url}>({urlLabel})</Link>)}
+                {url && urlList(Array.of(url).flat(), Array.of(urlLabel).flat())}
               </div>
             );
           })}
