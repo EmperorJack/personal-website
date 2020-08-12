1ef7ad166d0072023cbf127248f251647f892659
EmperorJack <emperorpurvis@gmail.com>
2020-08-12 15:46:19 +1200

    Project and performance updates:
    - Add recent limbic resonance performance entry
    - Refactor Projects page component by creating new Project component
      to represent individual projects
    - Add project entry for Maltopi Designer
    - Update descriptions of existing projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,58 +1,58 @@
 import React, { Fragment } from 'react';
 import Page from '../Page.jsx';
-import Link from '../shared/Link.jsx';
-import visor from '../../images/visor.png';
-import smokeSimulation from '../../images/smoke-simulation.jpg';
+import visorImagePath from '../../images/visor.png';
+import maltopiDesignerImagePath from '../../images/maltopi-designer.png';
+import smokeSimulationImagePath from '../../images/smoke-simulation.jpg';
+import Project from '../Project';
 
 const sections = {
-  'Current Projects': (<Fragment>
-    <div className="title">Visor</div>
+  'Current Projects': (
+    <Fragment>
+      <Project
+        title="Visor"
+        description="
+          Visor is a live coding environment for real-time visual performance. Visor bridges the gap
+          between creative coding and VJing by offering user interfaces to easily interact with live
+          coded Processing sketches. Visor offers a simple API for mapping visuals to audio or MIDI
+          controllers, all in the expressive Ruby programming language."
+        links={[
+          { label: 'Visor Website', url: 'https://visor-live.github.io/' },
+          { label: 'Demonstration Video', url: 'https://www.youtube.com/watch?v=oqSGJKDKigs' },
+        ]}
+        imagePath={visorImagePath}
+      />
 
-    <p>
-      Visor is a live coding environment for real-time visual performance. Visor bridges the gap
-      between creative coding and VJing by offering user interfaces to easily interact with live
-      coded Processing sketches. Visor offers a simple API for mapping visuals to audio or MIDI
-      controllers, all in the expressive Ruby programming language.
-    </p>
-
-    <div className="projects__link-container">
-      <Link url="https://visor-live.github.io/">Project Website</Link>
-      <Link url="https://www.youtube.com/watch?v=oqSGJKDKigs">Demonstration Video</Link>
-    </div>
-
-    <img
-      src={visor}
-      alt=""
-      className="projects__project-image"
+      <Project
+        title="Maltopi Designer"
+        description="
+          Maltopi Designer is a web-based clothing customiser for the sports team wear brand, Maltopi.
+          Maltopi Designer allows users to design football kits with a variety of customisations including
+          colours, patterns, and logo images. Once a kit is created, a quote can be requested from Maltopi
+          to produce the custom kit. Maltopi Designer is implemented using p5.js, React, and Firebase."
+        links={[
+          { label: 'Maltopi Designer Website', url: 'https://designer.maltopi.com/' },
+        ]}
+        imagePath={maltopiDesignerImagePath}
+      />
+    </Fragment>
+  ),
+  'Past Projects': (
+    <Project
+      title="Smoke Simulation for Real-time Music Visualisation"
+      description="
+        This project investigates how the analysis of live audio can be used to drive a real-time
+        smoke simulation, producing a music visualisation. To maximize performance the simulation is
+        implemented on the GPU. Vorticity confinement and buoyancy forces are implemented to improve
+        the smoke quality. Logarithmic frequency scaling and windowing are implemented to improve the
+        quality of the audio analysis. Multiple music visualisations are implemented to illustrate the
+        kind of effects that are possible when integrating audio analysis and a smoke simulation."
+      links={[
+        { label: 'Github Website', url: 'https://github.com/EmperorJack/smokey-bbq' },
+        { label: 'Demonstration Video', url: 'https://www.youtube.com/watch?v=Tsm45GY3AT4' },
+      ]}
+      imagePath={smokeSimulationImagePath}
     />
-  </Fragment>),
-  'Past Projects': (<Fragment>
-    <div className="title">Smoke Simulation for Real-time Music Visualisation</div>
-
-    <p>
-      Simulating smoke and other fluids for computer graphics is a challenging task. How can
-      such an effect be influenced by a dynamic input such as music to create an appealing
-      visualisation? This project investigates how the analysis of live audio can be used to
-      drive a real-time smoke simulation, producing a music visualisation. To maximize performance
-      the simulation is implemented on the GPU and the results are compared against a CPU
-      implementation. Vorticity confinement and buoyancy forces are implemented to improve
-      the smoke quality. Logarithmic frequency scaling and windowing are implemented and evaluated
-      to improve the quality of the audio analysis. Multiple music visualisations are implemented
-      to illustrate the kind of effects that are possible when integrating audio analysis and
-      a smoke simulation.
-    </p>
-
-    <div className="projects__link-container">
-      <Link url="https://github.com/EmperorJack/smokey-bbq">Github Website</Link>
-      <Link url="https://www.youtube.com/watch?v=Tsm45GY3AT4">Demonstration Video</Link>
-    </div>
-
-    <img
-      src={smokeSimulation}
-      alt=""
-      className="projects__project-image"
-    />
-  </Fragment>),
+  ),
 };
 
 const Projects = () => (
