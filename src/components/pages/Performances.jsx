ee42d36cd9e486d9c71c42fce2bafdb45254678d
EmperorJack <emperorpurvis@gmail.com>
2020-12-12 20:18:51 +1300

    Added camp art~hack performances & refactor performances component to support multiple titles

--- src/components/pages/Performances.jsx
+++ src/components/pages/Performances.jsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import Page from '../Page.jsx';
 import Link from '../shared/Link.jsx';
-import performances from '../../performances.json';
+import vjPerformances from '../../vjPerformances.json';
+import djPerformances from '../../djPerformances.json';
 
 /* eslint-disable quote-props */
 
-const urlList = (urls, urlLabels) => (
+const UrlList = ({ urls, urlLabels }) => (
   <Fragment>
     {urls.map((url, i) => (
       <Fragment key={url}>
@@ -16,34 +17,42 @@ const urlList = (urls, urlLabels) => (
   </Fragment>
 );
 
+const PerformanceList = ({ performances }) => (
+  <div className="list">
+    {Object.keys(performances).reverse().map((year) => (
+      <Fragment key={year}>
+        <div className="title">{year}</div>
+
+        {performances[year].map((performance) => {
+          const {
+            date, name, location, url, url_label: urlLabel,
+          } = performance;
+
+          return (
+            <div className="list-item" key={name}>
+              [{date}]
+              {' '}
+              <strong>{name}</strong>,
+              {' '}
+              {location}
+              {' '}
+              {url && (
+                <UrlList
+                  urls={Array.of(url).flat()}
+                  urlLabels={Array.of(urlLabel).flat()}
+                />
+              )}
+            </div>
+          );
+        })}
+      </Fragment>
+    ))}
+  </div>
+);
+
 const sections = {
-  'VISOR': (<Fragment>
-    <div className="list">
-      {Object.keys(performances).reverse().map((year) => (
-        <Fragment key={year}>
-          <div className="title">{year}</div>
-
-          {performances[year].map((performance) => {
-            const {
-              date, name, location, url, url_label: urlLabel,
-            } = performance;
-
-            return (
-              <div className="list-item" key={name}>
-                [{date}]
-                {' '}
-                <strong>{name}</strong>,
-                {' '}
-                {location}
-                {' '}
-                {url && urlList(Array.of(url).flat(), Array.of(urlLabel).flat())}
-              </div>
-            );
-          })}
-        </Fragment>
-      ))}
-    </div>
-  </Fragment>),
+  'VISOR': <PerformanceList performances={vjPerformances} />,
+  'DJACK (working title)': <PerformanceList performances={djPerformances} />,
 };
 
 const Performances = () => (
