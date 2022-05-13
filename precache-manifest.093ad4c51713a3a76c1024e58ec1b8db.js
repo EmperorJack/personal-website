d5fa8550c8105a758179371844718ab13caa14ad
EmperorJack <emperorpurvis@gmail.com>
2022-05-13 20:39:53 +1200

    Updates

--- precache-manifest.093ad4c51713a3a76c1024e58ec1b8db.js
+++ precache-manifest.093ad4c51713a3a76c1024e58ec1b8db.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd169d6a668ddb3efeca0d285361af5d",
+    "revision": "eb1b1341bafdde8411e28847223b8184",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22d6b5ec.chunk.css"
   },
   {
-    "revision": "d9d5979047a7685a1dc0",
+    "revision": "fb04492f374dd8ab861c",
     "url": "/static/css/main.ed3c60c4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.ff22f9b6.chunk.js"
   },
   {
-    "revision": "d9d5979047a7685a1dc0",
-    "url": "/static/js/main.2ed1a06c.chunk.js"
+    "revision": "fb04492f374dd8ab861c",
+    "url": "/static/js/main.f8c40d02.chunk.js"
   },
   {
     "revision": "f5c300d6359eb8c7b66d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1e8a6bc5c296cc4bef61708390bdcb3.js"
+  "/precache-manifest.093ad4c51713a3a76c1024e58ec1b8db.js"
 );
 
 self.addEventListener('message', (event) => {
