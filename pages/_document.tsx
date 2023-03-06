bbdfae3eb62984ef701ca75e4ceb99f8c274fb8b
EmperorJack <emperorpurvis@gmail.com>
2023-03-06 14:40:53 +1300

    Fix html tag lang value

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="end">
+    <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
